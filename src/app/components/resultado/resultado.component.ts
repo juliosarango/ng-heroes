526b93451aa52e85dc89bb9d743930f5daa5b391
Julio Sarango <jsarangoq@localhost.localdomain>
2017-05-24 17:14:23 -0500

    cambios presentacion de resultados de busqueda

--- src/app/components/resultado/resultado.component.ts
+++ src/app/components/resultado/resultado.component.ts
@@ -9,13 +9,18 @@ import { Heroe, HeroesService } from '../../services/heroes.service';
 export class ResultadoComponent implements OnInit {
 
   heroes: Heroe[] = [];
-  constructor( private activatedRoute:ActivatedRoute, heroesService:HeroesService ) {
-    this.activatedRoute.params.subscribe(
-      params => this.heroes = heroesService.buscarHeroes(params['termino'])
-    );    
+  termino: string;
+  constructor( private activatedRoute:ActivatedRoute, private heroesService:HeroesService ) {
   }
 
   ngOnInit() {
+      this.activatedRoute.params.subscribe(
+      params =>  {
+        let termino = params['termino'];
+        this.heroes = this.heroesService.buscarHeroes(termino);
+        this.termino = termino;
+      }
+    );
   }
 
 
